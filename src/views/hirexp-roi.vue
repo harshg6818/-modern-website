1a90c19a1f649413330202c9d7c5ddf209f42a97
Harsh Gupta <harshg6818@gmail.com>
2020-04-17 12:13:53 +0530

    fixing css and adding roi page

---
+++ src/views/hirexp-roi.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <section class="hirexp-roi-nav">
+      <nav class="navbar fixed-top navbar-expand-lg">
+        <div class="container">
+          <div class="hirexp-logo">
+            <img src="../assets/logo-light.png" alt="hirexp-logo" />
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkupRoi"
+            aria-controls="navbarNavAltMarkupRoi"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkupRoi">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link active" href="">Home</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </section>
+
+    <section class="roi-header-wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-5">
+            <div class="roi-header-left">
+              Annual Customer Value Survey 2019
+            </div>
+          </div>
+          <div class="col-7">
+            <div class="roi-bg-img">
+              <img
+                src="../assets/images/roi-bg.jpg"
+                alt="roi-bg"
+                width="100%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/_hirexp_roi.scss";
+</style>
