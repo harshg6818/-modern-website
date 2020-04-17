e48a50cfbe5cb30189597e8562537fa8dbd29118
Harsh Gupta <harshg6818@gmail.com>
2020-04-17 16:26:19 +0530

    adding roi page elements

--- src/views/hirexp-roi.vue
+++ src/views/hirexp-roi.vue
@@ -31,16 +31,324 @@
         <div class="row">
           <div class="col-5">
             <div class="roi-header-left">
-              Annual Customer Value Survey 2019
+              ROI Based on <br />CHROs Survey 2019
             </div>
           </div>
           <div class="col-7">
             <div class="roi-bg-img">
               <img
-                src="../assets/images/roi-bg.jpg"
+                src="../assets/images/roi/roi-bg.jpg"
                 alt="roi-bg"
                 width="100%"
               />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="hirexp-recruitment-software">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-recruitment-software-heading">
+              Recruitment Software
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <div class="product-heading">
+              Candidate Experience
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/candidate-experience.jpg"
+                alt="candidate-experience"
+                width="100%"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Recruiter Productivity
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/recruiter-productivity.jpg"
+                alt="recruiter-productivity"
+                width="180px"
+                height="180px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring Manager Engagement
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/hiring-manager-engagement.jpg"
+                alt="hiring-manager-engagement"
+                width="180px"
+                height="160px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring<br />
+              Velocity
+            </div>
+            <div class="product-details" style="margin-top:4rem">
+              <img
+                src="../assets/images/roi/graphic-velocity.jpg"
+                alt="graphic-velocity"
+                width="200px"
+                height="120px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="hirexp-amara-software">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-amara-software-heading">
+              Amara
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="hirexp-amara-software-heading-sub">
+              Engagement Experience Software
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <div class="product-heading">
+              Candidate Experience
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/candidate-experience.jpg"
+                alt="candidate-experience"
+                width="100%"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Recruiter Productivity
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/recruiter-productivity.jpg"
+                alt="recruiter-productivity"
+                width="180px"
+                height="180px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring Manager Engagement
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/hiring-manager-engagement.jpg"
+                alt="hiring-manager-engagement"
+                width="180px"
+                height="160px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring<br />
+              Velocity
+            </div>
+            <div class="product-details" style="margin-top:4rem">
+              <img
+                src="../assets/images/roi/graphic-velocity.jpg"
+                alt="graphic-velocity"
+                width="200px"
+                height="120px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="hirexp-another-software">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-recruitment-software-heading">
+              Recruitment Software
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <div class="product-heading">
+              Candidate Experience
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/candidate-experience.jpg"
+                alt="candidate-experience"
+                width="100%"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Recruiter Productivity
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/recruiter-productivity.jpg"
+                alt="recruiter-productivity"
+                width="180px"
+                height="180px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring Manager Engagement
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/hiring-manager-engagement.jpg"
+                alt="hiring-manager-engagement"
+                width="180px"
+                height="160px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring<br />
+              Velocity
+            </div>
+            <div class="product-details" style="margin-top:4rem">
+              <img
+                src="../assets/images/roi/graphic-velocity.jpg"
+                alt="graphic-velocity"
+                width="200px"
+                height="120px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="hirexp-another-software">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-recruitment-software-heading">
+              Recruitment Software
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-3">
+            <div class="product-heading">
+              Candidate Experience
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/candidate-experience.jpg"
+                alt="candidate-experience"
+                width="100%"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Recruiter Productivity
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/recruiter-productivity.jpg"
+                alt="recruiter-productivity"
+                width="180px"
+                height="180px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring Manager Engagement
+            </div>
+            <div class="product-details">
+              <img
+                src="../assets/images/roi/hiring-manager-engagement.jpg"
+                alt="hiring-manager-engagement"
+                width="180px"
+                height="160px"
+              />
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="product-heading">
+              Hiring<br />
+              Velocity
+            </div>
+            <div class="product-details" style="margin-top:4rem">
+              <img
+                src="../assets/images/roi/graphic-velocity.jpg"
+                alt="graphic-velocity"
+                width="200px"
+                height="120px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="hirexp-global-brands">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-global-brands-headings">
+              Global Brands Seeing Hiring Success With
+              <span style="color:#61245e">HireXP</span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="hirexp-global-brands-img">
+              <img
+                src="../assets/images/client-logo.png"
+                alt="global brands"
+                width="100%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="cta cta--default">
+      <div class="container">
+        <div class="get-started">
+          <h1 class="bold">Ready to get <span>started</span>?</h1>
+          <h3>Create a free account in just <strong>2 minutes</strong></h3>
+
+          <div class="get-started__actions">
+            <div class="get-started__action get-started__action--primary">
+              <a class="btn btn--primary btn--large" href="#">Get Started</a>
             </div>
           </div>
         </div>
